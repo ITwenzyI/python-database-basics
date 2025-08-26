f4981d4fd0f22664c7c6fd779f557adc883a8d9d
Kilian <kischoenhaar@gmail.com>
2025-08-26 18:59:36 +0200

    feat: add step03 with insert

---
+++ src/step03_insert.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect('../data/fitness.db')
+
+cur = conn.cursor()
+
+# cur.execute(
+#     "INSERT INTO workouts (workout_date, workout_type, workout_duration, workout_notes) VALUES (?, ?, ?, ?)",
+#
+#     ("2025-08-26", "Beine", 60, "Erstes Training gespeichert")
+# )
+
+cur.execute(
+    "INSERT INTO workouts (workout_date, workout_type, workout_duration) VALUES (?, ?, ?)",
+
+    ("2025-08-25", "Cardio", 80)
+)
+
+conn.commit()
+
+conn.close()
